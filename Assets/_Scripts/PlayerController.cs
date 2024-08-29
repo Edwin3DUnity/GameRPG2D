8840ea86fc0e22fbbc63e99a1ca18b9144905590
Edwin unity <eapaniagua8@misena.edu.co>
2024-08-29 11:08:13 -0500

    Movimiento del personaje

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField, Range(0, 15), Tooltip("Velocidad del personaje")]
+    private float speed = 5;
+
+    private const string AXIS_H = "Horizontal";
+    private const string AXIS_V = "Vertical";
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimientoPersnaje();
+    }
+
+    private void MovimientoPersnaje()
+    {
+        
+        //S = V*t
+        if (Mathf.Abs(Input.GetAxisRaw(AXIS_H)) > 0.2f)
+        {
+            Vector3 translation = new Vector3(Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime,0,0);
+            this.transform.Translate(translation);
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
+        {
+            Vector3 translation = new Vector3(0,Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
+            this.transform.Translate(translation);
+        }
+    }
+}
