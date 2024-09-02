39092f6fa13f6b350d087169b931e5bd4787c1b5
Edwin unity <eapaniagua8@misena.edu.co>
2024-09-02 09:13:50 -0500

    Movimiento del personaje por medio de rigidbody

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -20,14 +20,15 @@ public class PlayerController : MonoBehaviour
     private const string WALKING = "Walking";
     private const string LAST_H = "LastH";
     private const string LAST_V = "LastV";
-    
-    
+
+    private Rigidbody2D _rigidbody2D;
     
     // Start is called before the first frame update
     void Start()
     {
         
         _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -44,23 +45,35 @@ public class PlayerController : MonoBehaviour
         //S = V*t
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_H)) > 0.2f)
         {
+            /*
             Vector3 translation = new Vector3(Input.GetAxisRaw(AXIS_H) * speed * Time.deltaTime,0,0);
             this.transform.Translate(translation);
             walking = true;
-            lastmovement = new Vector2(Input.GetAxisRaw(AXIS_H),0);
+            lastmovement = new Vector2(Input.GetAxisRaw(AXIS_H),0);*/
+
+            _rigidbody2D.velocity = new Vector2(Input.GetAxisRaw(AXIS_H)* this.speed, _rigidbody2D.velocity.y);
+            this.walking = true;
+            lastmovement = new Vector2(Input.GetAxisRaw(AXIS_H), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw(AXIS_V)) > 0.2f)
         {
-            Vector3 translation = new Vector3(0,Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
+           /* Vector3 translation = new Vector3(0,Input.GetAxisRaw(AXIS_V) * speed * Time.deltaTime, 0);
             this.transform.Translate(translation);
             walking = true;
-            lastmovement = new Vector2(0, Input.GetAxisRaw(AXIS_V));
+            lastmovement = new Vector2(0, Input.GetAxisRaw(AXIS_V));*/
+           _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, Input.GetAxisRaw(AXIS_V) * this.speed);
+           this.walking = true;
+           lastmovement = new Vector2(0, Input.GetAxisRaw(AXIS_V));
         }
     }
 
     private void LateUpdate()
     {
+        if (!walking)
+        {
+            _rigidbody2D.velocity = Vector2.zero;
+        }
         _animator.SetFloat(AXIS_H, Input.GetAxisRaw(AXIS_H));
         _animator.SetFloat(AXIS_V, Input.GetAxisRaw(AXIS_V));
         _animator.SetBool(WALKING, walking);
